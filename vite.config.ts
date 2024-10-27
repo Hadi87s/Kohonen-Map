e292d22a8325fa8871bf8680997f37b91ce87dc6
Hadi Irshaid <HadiIrshaid8722@gmail.com>
2024-10-27 13:04:43 +0200

    Fixing some Bugs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    base: '/Kohonen-Map/',
     plugins: [react()],
+    base: '/Kohonen-Map/', // Add this line - must match your repository name
 });
-
-
